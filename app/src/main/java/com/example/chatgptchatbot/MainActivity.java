2635753ebff384a9da584a551c33aade165e3215
Diana-Kieru <dianakieru1@gmail.com>
2024-05-17 22:50:01 +0300

    fixed apikey

--- app/src/main/java/com/example/chatgptchatbot/MainActivity.java
+++ app/src/main/java/com/example/chatgptchatbot/MainActivity.java
@@ -94,11 +94,11 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-//        String apiKey = getResources().getString(R.string.openai_api_key);
+
         RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-//                .header("Authorization", "Bearer " + apiKey)
+                .header("Authorization", "Bearer ")
                 .post(body)
                 .build();
 
